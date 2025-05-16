2847f26ae1c9922d61aa123b2f5e91f6febf7748
zakzakst <e1433104@gmai.com>
2025-05-17 08:59:28 +0900

    修正

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  output: "export",
+  distDir: `out${process.env.NEXT_PUBLIC_BASE_PATH}`,
+  trailingSlash: true,
+  // SSGではnext/imageが利用できない。unoptimizedで割愛
+  images: {
+    unoptimized: true,
+  },
+  compiler: {
+    reactRemoveProperties:
+      process.env.NODE_ENV === "production"
+        ? { properties: ["^data-testid$"] }
+        : false,
+  },
 };
 
 export default nextConfig;
