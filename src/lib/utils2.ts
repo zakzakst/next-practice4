cb1c026078efca389342578d96735d8aaff95b91
zakzakst <e1433104@gmai.com>
2025-05-18 03:22:51 +0900

    修正

--- src/lib/utils2.ts
+++ src/lib/utils2.ts
@@ -1,3 +1,15 @@
 export const isObjectEmpty = (obj: Record<string, unknown>): boolean => {
   return Object.keys(obj).length === 0;
 };
+
+export const getRouterPushPath = (path: string) => {
+  // NOTE: ssgでbuildした際、router.pushを正しく動かすのにドメインを付ける必要があった
+  if (process.env.NODE_ENV === "production") {
+    const deployGitHub = true;
+    const domain = deployGitHub
+      ? "https://zakzakst.github.io"
+      : "http://localhost:3000";
+    return `${domain}${process.env.NEXT_PUBLIC_BASE_PATH}${path}`;
+  }
+  return path;
+};
