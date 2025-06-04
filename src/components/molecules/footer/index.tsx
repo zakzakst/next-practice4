18beb9c25e04e400dd439fd66f9d338d3f68576b
zakzakst <e1433104@gmai.com>
2025-06-04 19:59:11 +0900

    修正

--- src/components/molecules/footer/index.tsx
+++ src/components/molecules/footer/index.tsx
@@ -4,7 +4,7 @@ type Props = React.ComponentProps<"footer">;
 
 export const Footer = ({ className, ...rest }: Props) => {
   return (
-    <footer className={clsx("px-4 py-2 bg-gray-100", className)} {...rest}>
+    <footer className={clsx("bg-gray-100 px-4 py-2", className)} {...rest}>
       <p className="text-center">All rights reserved © XXXXX</p>
     </footer>
   );
